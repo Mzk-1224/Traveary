82ffc687cdf118d2f75445aefc3244423fa62497
Mizuki Muto Monis <mm@MzukMacBook-Air.local>
2024-05-29 15:43:16 +0900

    ユーザー設定と掲示板作成

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,8 +1,11 @@
 class Board < ApplicationRecord
   belongs_to :user
+  has_many :images, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
+
+  accepts_nested_attributes_for :images, allow_destroy: true
 
   paginates_per 20
 
