49c5bd97eaecf774623ec9a04819f91960d40460
Mizuki Muto Monis <mm@MzukMacBook-Air.local>
2024-05-31 15:11:21 +0900

    master_ker

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_173931) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_29_065846) do
   create_table "boards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "title", null: false
@@ -46,7 +46,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_173931) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "boards", "prefectures"
   add_foreign_key "boards", "users"
   add_foreign_key "images", "boards"
 end
